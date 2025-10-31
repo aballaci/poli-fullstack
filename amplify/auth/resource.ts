b2400dcf75e0273b42c97803ea6142d94121e4af
Armand Ballaci <aballaci@gmail.com>
2025-11-01 00:58:39 +0100

    added google auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,22 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+       externalProviders: {
+      // This is the key part for Google Sign-in
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['email', 'profile', 'openid'],
+      },
+      // You must also configure your callback URLs in the Google Console
+      callbackUrls: [
+        'http://localhost:4200/', // For local dev
+        'https://poli.ballaci.com/' // For production
+      ],
+      logoutUrls: [
+        'http://localhost:4200/',
+        'https://poli.ballaci.com/'
+      ]
+    },
   },
 });
