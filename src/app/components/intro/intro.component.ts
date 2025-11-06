1306d82178da19fdafb87c936c1905db84596f14
Armand Ballaci <aballaci@gmail.com>
2025-11-06 13:01:36 +0100

    updated intro

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
+  styleUrl: './intro.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, RouterLink]
+  imports: [CommonModule, RouterLink, HeaderComponent, FooterComponent]
 })
 export class IntroComponent { }
